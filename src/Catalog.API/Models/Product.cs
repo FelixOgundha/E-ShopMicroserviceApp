49cf27cd6b7efd076e50abf3568f7d8a91c9d27f
FelixOgundha <fogundha@strathmore.edu>
2024-08-13 16:56:17 +0300

    Add project files.

---
+++ src/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<string> Categories { get; set; } = new List<string>();
+        public string Description { get; set; } = string.Empty;
+        public Decimal Price { get; set; }
+    }
+}
---
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductHandler
+    {
+    }
+}
