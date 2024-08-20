939e520d0345e67a1768048eabc302a6defa2062
FelixOgundha <fogundha@strathmore.edu>
2024-08-20 14:25:16 +0300

    updated

--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name,List<string> Category,string ImageFile,decimal Price);
+    public record CreateProductCommand(string Name,List<string> Category,string ImageFile,decimal Price)
+        :IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
