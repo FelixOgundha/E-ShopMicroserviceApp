7fdc10270a656e4481757a069c73d1e064d189bb
fogundha <fogundha@strathmore.edu>
2024-08-27 17:57:21 +0300

    updated generic handler classes

---
+++ src/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery,TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,15 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 using Microsoft.AspNetCore.Authentication;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name,List<string> Category,string ImageFile,decimal Price)
-        :IRequest<CreateProductResult>;
+        :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProduct : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
