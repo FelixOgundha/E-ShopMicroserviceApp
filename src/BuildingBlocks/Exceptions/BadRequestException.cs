0e488ea5c8dd38e8bc6117c07df34e1d7c884adf
FelixOgundha <fogundha@strathmore.edu>
2024-09-19 17:58:47 +0300

    updated exceprtion handling

---
+++ src/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+
+        public BadRequestException(string message, string details) : base(message)
+        {
+            Details = details;
+        }
+
+        public string? Details { get; }
+    }
+}
---
+++ src/BuildingBlocks/Exceptions/InternalServerException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Exceptions
+{
+    public class InternalServerException : Exception
+    {
+        public InternalServerException(string message) : base(message)
+        {
+        }
+
+        public InternalServerException(string message, string details) : base(message)
+        {
+            Details = details;
+        }
+
+        public string? Details { get; }
+    }
+}
