dc4eb81ff75b94c50f7056c65737e8b498c23328
FelixOgundha <fogundha@strathmore.edu>
2024-10-08 16:02:27 +0300

    updated exception

--- src/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BuildingBlocks.Exceptions.Handler
 {
-    internal class CustomExceptionHandler
+    internal class CustomExceptionHandler (ILogger<CustomExceptionHandler> logger): IExceptionHandler
     {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(
+                $"Error Message: {exception.Message} ,Time of occurence {DateTime.UtcNow}"
+                );
+
+
+
+        }
     }
 }
--- src/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -4,9 +4,8 @@ namespace Catalog.API.Exceptions
 {
     public class ProductNotFoundException : NotFoundException
     {
-        public ProductNotFoundException(Guid Id): base("Product",Id)
+        public ProductNotFoundException(Guid Id) : base("Product", Id)
         {
         }
-
     }
 }
